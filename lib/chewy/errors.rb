ce2a01273d5a449f7aac842fd949d5ebf855ffa5
Giovanni Bonetti <giovanni.bonetti@gmail.com>
2017-01-05 11:17:24 -0200

    feature/better_error_messages -> anonymous errors

--- lib/chewy/errors.rb
+++ lib/chewy/errors.rb
@@ -29,11 +29,29 @@ Please wrap your code with `Chewy.strategy(:strategy_name) block.`
       errors.each do |action, errors|
         message << "    #{action.to_s.humanize} errors:\n"
         errors.each do |error, documents|
-          message << "      `#{error}`\n"
-          message << "        on #{documents.count} documents: #{documents}\n"
+          anonymous_error = anonymize_with_placeholders(error)
+          anonymous_documents = ['?']
+          message << "      `#{anonymous_error}`\n"
+          message << "        on #{anonymous_documents.count} documents: #{anonymous_documents}\n"
         end
       end
       super message
+    end
+
+    # Replace IDs and other specific data for placeholder '?'
+    # to anonymize the error and make it easier to group in the exception tracker
+    #
+    # Example input:
+    #   rejected execution of org.elasticsearch...Service$4@553f9b39 on EsThreadPoolExecutor[
+    #     ...capacity = 50, org.elasticsearch...EsThreadPoolExecutor@2134b86a[
+    #       ...pool size = 2, active threads = 2, queued tasks = 50, completed tasks = 6635921]]
+    #
+    # Example output:
+    #   rejected execution of org.elasticsearch...Service$? on EsThreadPoolExecutor[
+    #     ...capacity = ?, org.elasticsearch...EsThreadPoolExecutor@?[
+    #       ...pool size = 2, active threads = 2, queued tasks = ?, completed tasks = ?]]
+    def anonymize_with_placeholders(input)
+      input.to_s.gsub(/\b\d[@\w]+\b/, '?')
     end
   end
 
